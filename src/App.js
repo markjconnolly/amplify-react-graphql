f9f2a46de3d0c535bac3519df7d47fca9f42887e
Mark Connolly <mark.jude.connolly@gmail.com>
2023-11-03 20:18:16 -0400

    Help

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hell from V2</h1>
+        <h1>Help! I'm stuck in AWS and I can't get out!</h1>
       </header>
     </div>
   );
